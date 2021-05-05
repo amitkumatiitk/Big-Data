df9d785908bd564bd5b5dd0510d2f74e61b274c6
amitkumatiitk <83357329+amitkumatiitk@users.noreply.github.com>
2021-05-05 13:37:49 -0400

    Update BDM_HW4_Kumar.py

--- BDM_HW4_Kumar.py
+++ BDM_HW4_Kumar.py
@@ -69,7 +69,7 @@ for code,type_rst in zip(codes,type_list):
       .map(lambda x: (x[0],np.median(np.asarray(x[1])),np.std(np.asarray(x[1]))))\
       .map(lambda x: (x[0],x[1],x[1]+x[2],x[1]-x[2]))\
       .map(lambda x: non_zero(x))\
-      .map(lambda x: (x[0].year,x[0],x[1],x[2],x[3])).collect()
+      .map(lambda x: (x[0].year,x[0],x[1],x[2],x[3])).saveAsTextFile(type_rst)
   
   #header.union(rdd).saveAsTextFile(type_rst)
   
