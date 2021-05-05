c249d1dfc4c43a19290e7ac7cef19bfea25f8fec
amitkumatiitk <83357329+amitkumatiitk@users.noreply.github.com>
2021-05-05 13:31:55 -0400

    Update BDM_HW4_Kumar.py

--- BDM_HW4_Kumar.py
+++ BDM_HW4_Kumar.py
@@ -37,7 +37,7 @@ def non_zero(x):
     if x[3] > 0:
       return x
     else:
-      return (tuple([x[0],x[1],x[2],0]))
+      return tuple([x[0],x[1],x[2],0])
 
   #NYC_CITIES = set(['New York', 'Brooklyn', 'Queens', 'Bronx', 'Staten Island'])
   
@@ -69,7 +69,7 @@ for code,type_rst in zip(codes,type_list):
       .map(lambda x: (x[0],np.median(np.asarray(x[1])),np.std(np.asarray(x[1]))))\
       .map(lambda x: (x[0],x[1],x[1]+x[2],x[1]-x[2]))\
       .map(lambda x: non_zero(x))\
-      .map(lambda x: (x[0][:4],x[0],x[1],x[2],x[3])).saveAsTextFile(type_rst)
+      .map(lambda x: (x[0][:4],x[0],x[1],x[2],x[3])).collect()
   
   #header.union(rdd).saveAsTextFile(type_rst)
   
